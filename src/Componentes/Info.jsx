22ce11d7457de3aac7c3d7d273e6a78dc3d14d50
FerneyMurillo28 <ferneymurillo044@gmail.com>
2022-09-23 22:22:21 -0500

    Segunda actualizacion

--- src/Componentes/Info.jsx
+++ src/Componentes/Info.jsx
@@ -1,18 +1,18 @@
 export const Info=[
     {
         java:"Java",
-        img1:"https://lenguajes.vercel.app/static/media/Java.9fc767eb03662af9ede5.png"
+        img1:"https://cdn-icons-png.flaticon.com/512/226/226777.png"
     },
     {
         python:"Python",
-        img2:"https://ferney-murillo28-github-io-vxr5.vercel.app/static/media/Python.ecfb4e6faf26def45e95.png"
+        img2:"https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png"
     },
     {
         perl:"Perl",
-        img3:"https://lenguajes.vercel.app/static/media/Perl.25d3533e96b2f37fb8eb.png"
+        img3:"https://upload.wikimedia.org/wikipedia/commons/f/f0/Cebolla_Chulita.png"
     },
     {
         react:"React",
-        img4:"https://lenguajes.vercel.app/static/media/React.25803af39958a2cb9df1.png"
+        img4:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png"
     }
 ]
